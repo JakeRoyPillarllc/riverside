0049f6117ac1558f94fa07b9fbeab14c3c90a09a
Sikandar Shukla <sikandar.shukla@gmail.com>
2013-01-29 12:48:08 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Riverside::Application.config.secret_token = 'e2e5ced96bfe75c4b6f29b895c7f916287744b669c7703d026b570c709a9e48a68634cec40c0fd92f1f5ac51fe50fcbd58bfa1e23259f80e267f141172ea1399'
