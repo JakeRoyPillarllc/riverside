32baec61b6baf5595d27fdc39430ef133fcc5367
Sikandar Shukla <sikandar.shukla@gmail.com>
2013-02-06 22:10:34 -0500

    add dynamic footer partial, tweak logo and put in edit site info links for admins

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,26 @@
 class HomeController < ApplicationController
   def index
     @site = Site.first
+    @footercontent = "index"
   end
    
   def about
     @site = Site.first
+    @footercontent = "about"
+    
+    
   end
   
   def contact
     @site = Site.first
+    @footercontent = "contact"
   end
   
   def services
     @site = Site.first
+    @footercontent = "services"
   end 
   
 end
+
+
