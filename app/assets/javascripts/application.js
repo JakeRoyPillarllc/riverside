a495963214645e267d667601b46aa3bc37ce1bc4
bhaity <bhaity@gmail.com>
2013-12-02 18:27:09 -0500

    responsive image resizing, no image distortion..

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,20 +15,20 @@
 //= require bootstrap
 //= require_tree .
 
-// $(window).load(function() {
-//   $(".infobar").css("height", $(".content").height()-100);
-// });
+$(window).load(function() {
+  $(".infobar").css("height", $(".content").height()-100);
+});
 
 
 
-// ch = $(".content").height();
-// $(window).resize(function(){
+ch = $(".content").height();
+$(window).resize(function(){
 	
-// 	if (ch != $(".content").height()){
-// 		$(".infobar").css("height", $(".content").height()-100);
-// 	}
+	if (ch != $(".content").height()){
+		$(".infobar").css("height", $(".content").height()-100);
+	}
 
-// });
+});
 
 
   function setContainerHeightToWindowHeight(){
