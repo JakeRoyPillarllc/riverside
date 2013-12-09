c02bab4f11c1c7108bf5dcc9c35134a67ab71a05
bhaity <bhaity@gmail.com>
2013-12-09 12:12:16 -0500

    include site in projects page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,6 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
+    @site = Site.first
     @projects = Project.all
     @footercontent = 'projects'
 
